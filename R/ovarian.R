4b64cd921ff713fc708257a32402dcec3da7aab8
Luca Braglia <lbraglia@gmail.com>
2016-11-18 16:58:13 +0100

    update

--- R/ovarian.R
+++ R/ovarian.R
@@ -14,13 +14,16 @@
 #'  \describe{
 #'    \item{\code{npaz}}{patient id}
 #'    \item{\code{tures}}{residual tumor size (1 = absent, 2 = micro, 3 = <= 1cm, 4 = 1-2cm, 5 = 2-5cm, 6 = 5-10cm, 7 = > 10 cm}
-#'    \item{\code{kar}}{Karnofsky Index}
+#'    \item{\code{tures_cl}}{residual tumor size classes}
+#' \item{\code{kar}}{Karnofsky Index}
+#' \item{\code{kar_cl}}{Karnofsky Index classes}
 #'    \item{\code{histo}}{Histology}
+#'    \item{\code{histo_cl}}{Histology classes}
 #'    \item{\code{figo}}{Federation Internationale de Gynecologie et d'Obstetrique with stages I, II, III}
-#'    \item{\code{status}}{??}
-#'    \item{\code{treat}}{treatment group (1 = CAP, 2 = CP, 3 = P)}
+#'    \item{\code{treatment}}{treatment group (1 = CAP, 2 = CP, 3 = P)}
 #'    \item{\code{surv}}{event indicator (0 = censored, 1 = death)}
 #'    \item{\code{survtime}}{time to event (days)}
+#'    \item{\code{status}}{??}
 #' }
 #' @examples
 #' head(ovarian)
--- rawdata/ovarian.R
+++ rawdata/ovarian.R
@@ -3,8 +3,42 @@ ovarian <- read.table(file = "ovarian.txt",
                       na.strings = '.',
                       row.names = NULL)
 
-## ovarian$turesc <- with(ovarian, ifelse(tures <= 4, 0, ifelse(tures >= 7, 2, 1)))
-## ovarian$karc <- with(ovarian, ifelse(kar <= 70, 0, 1))
-## ovarian$histoc <- with(ovarian, ifelse(histo == 1, 0, ifelse(histo == 7, 0, 1)))
+## tures class
+t_cl <- with(ovarian, ifelse(tures <= 4, 0, ifelse(tures >= 7, 2, 1)))
+t_labs <- c('<= 2cm', '2-10cm', '> 10cm')
+ovarian$tures_cl <- factor(t_cl, levels = 0: 2, labels = t_labs)
 
+## karnofsky class
+ovarian$kar_cl <- factor(ovarian$kar > 70, levels = c(FALSE, TRUE),
+                         labels = c('<= 70', '> 70'))
+
+## histology class
+h_cl <- with(ovarian, ifelse(histo %in% c(0, 1), 0L, 1L))
+h_labs <- c('serous', 'not serous')
+ovarian$histo_cl <- factor(h_cl, levels = 0:1, labels = h_labs)
+
+## treatment (renaming and factor)
+names(ovarian)[names(ovarian) %in% 'treat'] <- 'treatment'
+ovarian$treatment <- factor(ovarian$treatment,
+                            levels = 1:3,
+                            labels = c('CAP', 'CP', 'P'))
+
+
+## figo factor
+ovarian$figo <- factor(ovarian$figo)
+
+var_order <- c("npaz",
+               "tures",
+               "tures_cl",
+               "kar",
+               "kar_cl",
+               "histo",
+               "histo_cl",
+               "figo",
+               "treatment",
+               "surv",
+               "survtime",
+               "status")
+
+ovarian <- ovarian[, var_order]
 save("ovarian", file = "../data/ovarian.rda", compress = "bzip2")
